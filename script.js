28f2c2239c9b3d0e2ffe5e919f34b98a9d82dfb8
Soi Thangsing <getdatasoi@outlook.com>
2024-10-09 09:09:19 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let btn = document.getElementById('lib-button')
+let sidebar = document.querySelector('.nav-left')
+
+btn.onclick = () => {
+    sidebar.classList.toggle('active')
+}
+
+// Make a timer to set disappear for scrollbar after some time and reappear when hover above it
+
+// Make tooltips
